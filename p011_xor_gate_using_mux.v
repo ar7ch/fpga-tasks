b84f49487572182384ae190de66e878ebc6d170e
Artem Bulgakov <artyombulgakov@gmail.com>
2021-11-22 02:27:36 +0300

    add task p011

---
+++ p011_xor_gate_using_mux.v
@@ -0,0 +1,64 @@
+module mux
+(
+  input  d0, d1,
+  input  sel,
+  output y
+);
+
+  assign y = sel ? d1 : d0;
+
+endmodule
+
+//----------------------------------------------------------------------------
+
+module xor_gate_using_mux
+(
+    input  a,
+    input  b,
+    output o
+);
+
+  // TODO
+
+  // Implement xor gate using instance(s) of mux,
+  // constants 0 and 1, and wire connections
+  wire t1, t2;
+  mux u0(0, 1, a, t1);
+  mux u1(1, 0, a, t2);
+  mux u2(t1, t2, b, o);
+
+endmodule
+
+//----------------------------------------------------------------------------
+
+module testbench;
+
+  logic a, b, o;
+  int i, j;
+
+  xor_gate_using_mux inst (a, b, o);
+
+  initial
+    begin
+      for (i = 0; i <= 1; i++)
+      for (j = 0; j <= 1; j++)
+      begin
+        a = i;
+        b = j;
+
+        # 1;
+
+        $display ("TEST %b ^ %b = %b", a, b, o);
+    
+        if (o !== (a ^ b))
+          begin
+            $display ("%s FAIL: %h EXPECTED", `__FILE__, a ^ b);
+            $finish;
+          end
+      end
+      
+      $display ("%s PASS", `__FILE__);
+      $finish;
+    end
+
+endmodule
